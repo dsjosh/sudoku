bc99b154cd0b7ee984cc070bf0431454c15c1984
dsjosh <43461340+dsjosh@users.noreply.github.com>
2026-05-01 14:20:52 +0800

    added way to pass in grid for TDD

--- string_constants.py
+++ string_constants.py
@@ -1,9 +1,33 @@
 
 class StringConstants:
 
-    APP_NAME = "Sudoku"
+    WELCOME = "Welcome to Sudoku!\n\nHere is your puzzle:"
     PROMPT = "Enter command (e.g., A3 4, C5 clear, hint, check, quit):\n"
+    ACCEPT = "Move accepted.\n"
+    GOOD = "No rule violations detected.\n"
+    CURRENT = "Current grid:"
+    SUCCESS = "You have successfully completed the Sudoku puzzle!\nPress any key to play again..."
+    QUIT = "Quitting the game!\nHave a nice day..."
+    HINT_INPUT = "hint"
+    CHECK_INPUT = "check"
+    QUIT_INPUT = "quit"
 
     @staticmethod
-    def user_greeting(username: str) -> str:
-        return f"Hello, {username}! Welcome to {StringConstants.APP_NAME}."
+    def prefill(cell: str) -> str:
+        return f"Invalid move. {cell} is pre-filled."
+
+    @staticmethod
+    def hint(cell: str, number: str) -> str:
+        return f"Hint: Cell {cell} = {number}"
+
+    @staticmethod
+    def exist_row(row: str, number: str) -> str:
+        return f"Number {number} already exists in Row {row}."
+
+    @staticmethod
+    def exist_col(col: str, number: str) -> str:
+        return f"Number {number} already exists in Column {col}."
+
+    @staticmethod
+    def exist_subgrid(number: str) -> str:
+        return f"Number {number} already exists in the same 3×3 subgrid."
--- sudoku.py
+++ sudoku.py
@@ -2,22 +2,31 @@ from string_constants import StringConstants
 import sys
 from collections import deque
 
-print(StringConstants.user_greeting("Josh"))
+def parse_args():
+    args = sys.argv[1:]
+    if args and len(args[0]) == 81:
+        grid = args[0]
+        commands = deque(args[1:])
+    else:
+        grid = None
+        commands = None
+    return grid, commands
 
 def process_single_sudoku_command(command: str):
     print(f"Processing command: {command}")
 
 def main():
-    commands = deque(sys.argv[1:])
-    interactive = not commands
-    if interactive:
+    print(StringConstants.WELCOME)
+    grid, commands = parse_args()
+    interactive = grid is None
+    if interactive: #For actual user
         while True:
             command = input(StringConstants.PROMPT).strip()
-            if command == "quit":
-                break
+            if command == StringConstants.QUIT_INPUT:
+                print(StringConstants.QUIT)
             if command:
                 process_single_sudoku_command(command)
-    else:
+    else: #For TDD
         while commands:
             process_single_sudoku_command(commands.popleft())
 
