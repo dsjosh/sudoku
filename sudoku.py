7059d40f29e8b0ce3341c9beed287c1a3d6283fa
dsjosh <43461340+dsjosh@users.noreply.github.com>
2026-05-03 14:54:56 +0800

    added skeleton for print_grid

--- sudoku.py
+++ sudoku.py
@@ -5,23 +5,20 @@ from collections import deque
 def parse_args():
     args = sys.argv[1:]
     if args and len(args[0]) == 81:
-        grid = args[0]
+        grid_raw = args[0]
         commands = deque(args[1:])
     else:
-        grid = None
+        grid_raw = None
         commands = None
-    return grid, commands
+    return grid_raw, commands
 
 def generate_grid():
     grid = []
     base_row = [1,2,3,4,5,6,7,8,9]
-    for band_start in range(3):
-        # create the first row of this band
+    for band_start in range(3): #repeat 3 times (3 rows per loop) to complete the set of 9 rows total
         row = base_row[band_start:] + base_row[:band_start]
-        # now generate 3 rows in this band (all shifts of 3)
-        for _ in range(3):
+        for _ in range(3): #repeat 3 times (1 row per loop) to complete the set of 3 rows
             grid.append(row[:])
-            # shift by 3 every time (your rule)
             front = row[:3]
             back = row[3:]
             row = back + front
@@ -31,8 +28,6 @@ def grid_to_string(grid):
     return "".join(str(cell) for row in grid for cell in row)
 
 def string_to_grid(s):
-    if len(s) != 81:
-        raise ValueError("String must be exactly 81 characters")
     grid = []
     for i in range(0, 81, 9):
         row = [int(ch) for ch in s[i:i+9]]
@@ -42,20 +37,27 @@ def string_to_grid(s):
 def process_single_sudoku_command(command: str):
     if command == StringConstants.QUIT_INPUT:
         print(StringConstants.QUIT)
+        sys.exit(0)
     else:
         print(f"Processing command: {command}")
 
+def print_grid(grid):
+    pass
+
 def main():
     print(StringConstants.WELCOME)
-    grid, commands = parse_args()
-    interactive = grid is None
+    grid_raw, commands = parse_args()
+    interactive = grid_raw is None
     if interactive: #For actual user
         grid = generate_grid()
+        print_grid(grid)
         while True:
             command = input(StringConstants.PROMPT).strip()
             if command:
                 process_single_sudoku_command(command)
     else: #For TDD
+        grid = string_to_grid(grid_raw)
+        print_grid(grid)
         while commands:
             process_single_sudoku_command(commands.popleft())
 
